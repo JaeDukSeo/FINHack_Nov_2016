a970324175bb9cc734149d1ddb9c746a1d1c8592
jdseo <jae.duk.seo@ryerson.ca>
2016-11-19 07:21:16 -0500

    Added model

--- FINHack_Django/FINHack_Django/urls.py
+++ FINHack_Django/FINHack_Django/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
     url(r'^$', 'FINHack_mainapp.views.home', name='home'),
+    
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- FINHack_Django/FINHack_mainapp/views.py
+++ FINHack_Django/FINHack_mainapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(requests):
-	return render(requests,'home.html',{})
+	return render(requests,'index.html',{})
