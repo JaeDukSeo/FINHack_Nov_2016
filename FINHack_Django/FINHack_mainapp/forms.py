9917f68dd39cbfb5bbaf8c8f6fae8f68b167ad93
jdseo <jae.duk.seo@ryerson.ca>
2016-11-19 09:55:55 -0500

    Finished up and start to create cisco

--- FINHack_Django/FINHack_mainapp/forms.py
+++ FINHack_Django/FINHack_mainapp/forms.py
@@ -5,7 +5,7 @@ from .models import Mainapp ,CreateRoom
 class MainappForm(forms.ModelForm):
 	class Meta:
 		model = Mainapp
-		fields = ['email'] 
+		fields = ['email']
 
 	# Nov 19 - can filter out, if the user exist within the cisco spark app
 	def clean_email(self):
@@ -20,7 +20,10 @@ class MainappForm(forms.ModelForm):
 class CreateRoomForm(forms.ModelForm):
 	class Meta:
 		model = CreateRoom
-		fields = ['room_name'] 
+		fields = ['room_name']
+        widgets = {
+            'room_name': forms.TextInput(attrs={'background-color': 'gray'}),
+        }
 
 	# Nov 19 - can filter out, if the user exist within the cisco spark app
 	def clean_room_name(self):
